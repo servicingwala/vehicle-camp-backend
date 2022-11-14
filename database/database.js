84d0af883c4ef2f4907ef581172f531b51dacb44
Pratik Dhoke <pratikdhoke13@gmail.com>
2022-11-14 22:49:54 +0530

    changed databse to mysql

--- database/database.js
+++ database/database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database/dev.sqlite",
-});
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./database/dev.sqlite",
+// });
 
+const sequelize = new Sequelize(
+  "mysql://uijdqvce85yzcmwk:4Wb2rq4LnvFXGhnuOs0a@bbfftpj38aeuvanvn7jj-mysql.services.clever-cloud.com:3306/bbfftpj38aeuvanvn7jj"
+);
 module.exports = sequelize;
